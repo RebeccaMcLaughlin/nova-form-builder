c771b0b7908617887108e2646393195d6530680e
Becca <becca.mclaughlin@laser.red>
2020-02-05 21:47:48 +0000

    Initial commit

---
+++ src/FormBuilder.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello.
+    </div>
+</template>
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+    components: {
+        draggable,
+    },
+    
+}
+</script>
